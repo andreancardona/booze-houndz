1496e569debd82563f372b4656c538434bfaf1ec
github username <github email address>
2018-01-22 17:33:04 -0500

    seeded database

--- db/migrate/20180122183616_create_liquor_stores.rb
+++ db/migrate/20180122183616_create_liquor_stores.rb
@@ -1,12 +1,16 @@
 class CreateLiquorStores < ActiveRecord::Migration[5.1]
   def change
     create_table :liquor_stores do |t|
+      t.integer :neighborhood_id
       t.string :name
       t.string :image
-      t.float :latitude_coordinate
+      t.string :latitude_coordinate
       t.string :longitude_coordinate
-      t.string :float
       t.string :price
+      t.string :address1
+      t.string :address2
+      t.string :city
+      t.string :state
       t.string :zip_code
       t.string :display_address
       t.string :display_phone
