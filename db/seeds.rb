636b76dda61e39d665523d9d8b2bc27b0d9b1884
github username <github email address>
2018-01-23 15:29:29 -0500

    eureka - dammnit - got our data back

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,20 +52,24 @@ def create_liquor_stores
 
     parsed_array.each do |attribute|
       neighborhood = Neighborhood.find_by(name:[attribute["location"]["zip_code"]])
-      ls_name = attribute["name"]
-      ls_image = attribute["image_url"]
-      ls_latitude_coordinate = attribute["coordinates"]["latitude"]
-      ls_longitude_coordinate = attribute["coordinates"]["longitude"]
-      ls_price = attribute["price"]
-      ls_address1 = attribute["location"]["address1"]
-      ls_address2 = attribute["location"]["address2"]
-      ls_city = attribute["location"]["city"]
-      ls_state = attribute["location"]["state"]
-      ls_zip_code = attribute["location"]["zip_code"]
-      ls_display_address = attribute["location"]["display_address"]
-      ls_display_phone = attribute["display_phone"]
 
-      LiquorStore.create(neighborhood_id: neighborhood.id, name: ls_name, image: ls_image, latitude_coordinate: ls_latitude_coordinate, longitude_coordinate: ls_longitude_coordinate, price: ls_price, address1: ls_address1, address2: ls_address2, city: ls_city, state: ls_state, zip_code: ls_zip_code, display_address: ls_display_address, display_phone: ls_display_phone)
+      if neighborhood
+        ls_name = attribute["name"]
+        ls_image = attribute["image_url"]
+        ls_latitude_coordinate = attribute["coordinates"]["latitude"]
+        ls_longitude_coordinate = attribute["coordinates"]["longitude"]
+        ls_price = attribute["price"]
+        ls_address1 = attribute["location"]["address1"]
+        ls_address2 = attribute["location"]["address2"]
+        ls_city = attribute["location"]["city"]
+        ls_state = attribute["location"]["state"]
+        ls_zip_code = attribute["location"]["zip_code"]
+        ls_display_address = attribute["location"]["display_address"]
+        ls_display_phone = attribute["display_phone"]
+
+        LiquorStore.find_or_create_by(neighborhood_id: neighborhood.id, name: ls_name, image: ls_image, latitude_coordinate: ls_latitude_coordinate, longitude_coordinate: ls_longitude_coordinate, price: ls_price, address1: ls_address1, address2: ls_address2, city: ls_city, state: ls_state, zip_code: ls_zip_code, display_address: ls_display_address, display_phone: ls_display_phone)
+      end
+
     end
   end
 end
