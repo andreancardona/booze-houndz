55df80395d1b7739d25cee567c6512e0c5d85410
github username <github email address>
2018-01-23 11:48:42 -0500

    added reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
                                  dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+
+  has_many :reviews
+  has_many :liquor_stores, through: :reviews
 end
