3820e63f535e9ebfd3ade2509b0af115fe66d7d8
github username <github email address>
2018-01-24 10:47:19 -0500

    links to followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,26 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: "Relationship",
                                  foreign_key: "followed_id",
                                  dependent: :destroy
+
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
   has_many :reviews
   has_many :liquor_stores, through: :reviews
+
+
+
+  def follow(other_user)
+    following << other_user
+  end
+
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,15 @@ Rails.application.routes.draw do
   resources :neighborhoods, only: [:index, :show]
   resources :liquor_stores, only: [:index, :show]
   resources :users, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-  # resources :user do
-  #   member do
-  #     get :following, :followers
-  #   end
-  # end
+  
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # resources :relationships, only: [:create, :destroy]
-  
+
   get "signup", to: "users#new", as: "signup"
   get "login", to: "sessions#new", as: "login"
   post "sessions", to: "sessions#create", as: "sessions"
