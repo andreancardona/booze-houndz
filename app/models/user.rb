e09645c4b9fa13a4ad60d11f3440d5c82450ba43
github username <github email address>
2018-01-24 09:53:01 -0500

    everything is working - but we still need follower stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  
+
   has_many :active_relationships, class_name: "Relationship",
                                  foreign_key: "follower_id",
                                  dependent: :destroy
