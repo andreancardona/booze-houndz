cccc519d6b65146eb853c5a243128ed8335084f2
github username <github email address>
2018-01-25 08:26:37 -0500

    avg rating

--- app/models/liquor_store.rb
+++ app/models/liquor_store.rb
@@ -2,4 +2,16 @@ class LiquorStore < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
   belongs_to :neighborhood
+
+  def average_rating
+    ratings_sum = 0
+    revs = self.reviews
+    ratings_array = revs.map {|r| r.rating}
+
+    ratings_array.each do |r|
+      ratings_sum += r
+    end
+    ratings_sum / ratings_array.length
+  end
+
 end
